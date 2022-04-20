4f55359de1e2329cd58387561bec2ea9ae6070d6
전민우 <mw.jeon@lifesemantics.kr>
2022-04-20 17:40:26 +0900

    Bean Validation 추가

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,29 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+// @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 이상이어야 합니다.")
+// global error 처리 > 기능이 제한적이라 비추천(java 코드 처리)
 public class Item {
 
+    @NotNull(groups = {UpdateCheck.class})
     private Long id;
+
+    @NotBlank(message = "item의 이름은 NULL일 수 없습니다.", groups = {SaveCheck.class, UpdateCheck.class})
     private String itemName;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    @Max(value = 9999, groups={SaveCheck.class})
     private Integer quantity;
 
     public Item() {
@@ -18,4 +34,5 @@ public class Item {
         this.price = price;
         this.quantity = quantity;
     }
+
 }
