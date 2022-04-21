73720bc3912f74ab94c6849367ac664e1032eb9c
walker0625 <jeonminwoo0625@gmail.com>
2022-04-21 22:03:39 +0900

    Response Validation 추가

---
+++ src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,28 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.web.validation.form.ItemSaveForm;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/validation/api/items")
+public class ValidationItemApiController {
+
+    @PostMapping("/add")
+    public Object addItem(@RequestBody @Validated ItemSaveForm form,
+                          BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return bindingResult.getAllErrors();
+        }
+
+        return form;
+    }
+
+}
